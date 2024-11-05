ae1305f97d13b03072a4a923e2b75809dc71ae63
Nadya Rakmeni <S22129049@student.unklab.ac.id>
2024-11-05 19:29:38 +0700

    Tambahkan File App.js & index.js

---
+++ naredi/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
